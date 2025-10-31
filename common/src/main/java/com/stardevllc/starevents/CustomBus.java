b0037cb1337d6c5348ce575e89a1b78f89931d35
Firestar311 <slaterandrew0398@gmail.com>
2025-10-30 19:28:20 -0500

    Fixed high tick usage

--- common/src/main/java/com/stardevllc/starevents/CustomBus.java
+++ common/src/main/java/com/stardevllc/starevents/CustomBus.java
@@ -3,27 +3,40 @@ package com.stardevllc.starevents;
 import com.stardevllc.starlib.eventbus.impl.SimpleEventBus;
 import org.bukkit.event.Event;
 
+import java.time.Instant;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class CustomBus extends SimpleEventBus<Event> {
     
-    private final Map<Event, Long> recentEvents = new HashMap<>();
+    private final Map<Event, Instant> recentEvents = new ConcurrentHashMap<>();
     
     public CustomBus() {
         super(Event.class);
+        
+        Thread.ofVirtual().start(() -> {
+            try {
+                Thread.sleep(30000);
+            } catch (InterruptedException e) {}
+            
+            Instant now = Instant.now();
+            new HashMap<>(recentEvents).forEach((event, expire) -> {
+                if (now.isAfter(expire)) {
+                    recentEvents.remove(event);
+                }
+            });
+        });
     }
     
     @Override
     public <E extends Event> E post(E event) {
-        //Remove the event instance after 1 second
-        recentEvents.entrySet().removeIf(entry -> System.currentTimeMillis() >= entry.getValue() + 1000L);
-        
         if (recentEvents.containsKey(event)) {
             return event;
         }
         
-        recentEvents.put(event, System.currentTimeMillis());
+        Instant currentTime = Instant.now();
+        recentEvents.put(event, currentTime.plusSeconds(1));
         return super.post(event);
     }
 }
